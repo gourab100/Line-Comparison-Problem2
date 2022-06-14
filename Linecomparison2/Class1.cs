43fb4c1795d3e881afb9e11709401f6ea0e87cb4
gourab <gourab.saheb100@gmail.com>
2022-06-14 18:39:21 +0530

    Add: Welcome to Line Comparison Problem

---
+++ Linecomparison2/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linecomparison2
+{
+    internal class Class1
+    {
+        public  double  lengthofline(int x1,int x2,int y1,int y2)
+        {
+
+             double lengthofline=(double)Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            return (double)lengthofline;
+        }
+    }
+}
---
+++ Linecomparison2/Program.cs
@@ -0,0 +1,10 @@
+﻿using Linecomparison2;
+Class1 line= new Class1();
+
+double line1= line.lengthofline(2, 4, 6, 7);
+double line2=line.lengthofline(12, 53, 19, 42);
+
+Console.WriteLine(line1);
+Console.WriteLine(line2);
+
+
